90f19b1bf67647bb4176ffdefbaeacb7a0fd2626
rjigyesh7 <rtdjigyesh777@gmail.com>
2020-12-29 20:19:44 +0530

    Added execution methods

---
+++ src/main/java/com/app/spring/mongo/api/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.app.spring.mongo.api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.app.spring.mongo.api.model.Book;
+
+public interface BookRepository extends MongoRepository<Book, Integer> {
+
+}
